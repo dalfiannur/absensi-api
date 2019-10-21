1f9db64b90e2cb1c1c8b591ff83def241851ca11
Dikry Alfiannur <dalfiannur@localhost.localdomain>
2019-10-21 23:26:49 +0700

    Fix AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { dirname } from 'path';
-import { UserRoleModule } from './user-role/user-role.module';
-import { UserModule } from './user/user.module';
-import { PresenceModule } from './presence/presence.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { Connection } from 'typeorm';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserRoleModule } from './user-role/user-role.module'
+import { UserModule } from './user/user.module'
+import { PresenceModule } from './presence/presence.module'
+import { Connection } from 'typeorm'
 
 @Module({
   imports: [
@@ -19,19 +17,14 @@ import { Connection } from 'typeorm';
       password: '1sampai8',
       database: 'absensi',
       entities: ['**/*.entity.ts'],
-      synchronize: true,
-    }),
-    MulterModule.registerAsync({
-      useFactory: () => ({
-        dest: '/uploads',
-      }),
+      synchronize: true
     }),
     UserRoleModule,
     UserModule,
-    // PresenceModule,
+    PresenceModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {
   constructor(private readonly connection: Connection) {}
